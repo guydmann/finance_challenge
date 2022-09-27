841801ba1458786be1f8f6ed49bebe1b0a9a5ea9
Guy Mann <guydmann@gmail.com>
2022-09-26 20:33:31 -0400

    fixed bug with data collection not getting the last day of july

--- collect_data.py
+++ collect_data.py
@@ -2,7 +2,7 @@ import yfinance as yf
 
 
 def collect_data():
-    data = yf.download("META AMZN NFLX GOOG", start="2017-06-30", end="2017-07-31")
+    data = yf.download("META AMZN NFLX GOOG", start="2017-06-30", end="2017-08-01")
     print(data["Adj Close"])
     data["Adj Close"].to_csv("MANG_adj_close.csv")
 
